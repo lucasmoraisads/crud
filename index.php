0545b32bd743280e44a3b338fd50a79e31b327ba
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-07-24 21:03:05 -0300

    style com boostrap 5

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet" >
+    <title>Home</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="https://github.com/lucasmoraisads">Lucas/GitHub</a>
+        
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                <a class="nav-link" href="cadastro.php">Cadastro </a>
+                <a class="nav-link" href="login.php">Login</a>
+                <a class="nav-link" href="leia.php">Leia mais</a>
+            </div>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+       
+    </div>
+    <script src="js/bootstrap.bundle.min.js" ></script>
+</body>
+</html>
