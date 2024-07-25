789df7c97d1457cb9b7b2aa58327b09f7ebd2ffa
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-07-24 22:02:09 -0300

    pagina de cadastro

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/bootstrap.min.css" rel="stylesheet" >
-    <title>Cadastro</title>
+    <title>Login</title>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -23,12 +23,9 @@
     </nav>
     
     <div class="container">
-       <h2>Formulario</h2>
-       <form action="cadastra.php" method="POST">
-        <div class="mb-3">
-            <label for="nome">Nome</label>
-            <input type="text" name="nome">
-        </div>
+       
+       <form action="">
+       <h2>Login</h2>
         <div class="mb-3">
             <label for="email">Email</label>
             <input type="email" name="email">
@@ -37,7 +34,7 @@
             <label for="nome">Senha</label>
             <input type="password" name="senha">
         </div>
-        <button type="submit" class="btn btn-primary">CADASTRAR</button>
+        <button type="submit" class="btn btn-primary">Logar</button>
        </form>
     </div>
     <script src="js/bootstrap.bundle.min.js" ></script>
