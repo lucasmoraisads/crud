3d4e3983d38f85a3be77ff2211cf819e73d6ce79
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-07-24 21:35:48 -0300

    pagina de cadastro

---
+++ cadastro.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet" >
+    <title>Home</title>
+</head>
+<body>
+    
+    <div class="container">
+       <h2>Formulario</h2>
+       <form action="">
+        <div class="mb-3">
+            <label for="nome">Nome</label>
+            <input type="text" name="nome">
+        </div>
+        <div class="mb-3">
+            <label for="email">Email</label>
+            <input type="email" name="email">
+        </div>
+        <div class="mb-3">
+            <label for="nome">Senha</label>
+            <input type="password" name="senha">
+        </div>
+        <button type="submit" class="btn btn-primary">CADASTRAR</button>
+       </form>
+    </div>
+    <script src="js/bootstrap.bundle.min.js" ></script>
+</body>
+</html>
